e5256fa1f6c724f1a915e8baf6bc385851fc5723
Phelipe <phelipe.silva@sptech.school>
2025-05-31 01:59:36 -0300

    arrumando endpoint do login e constraint da tabela

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -29,7 +29,7 @@ const Usuario = sequelize.define('Usuario', {
     type: DataTypes.STRING(45),
     allowNull: false,
     validate: {
-      isIn: [['Admin', 'Padrão']]
+      isIn: [['admin', 'padrao']]
     }
   }
 }, {
